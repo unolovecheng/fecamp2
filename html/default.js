161969b20177ba1f0a7a71ce9f5974b771c64f7c
unolovecheng <unolovecheng@gmail.com>
2015-02-28 15:37:52 +0900

    active 클래스 지정

--- html/default.js
+++ html/default.js
@@ -1,7 +1,14 @@
 jQuery(function($){
-	$(".gnb>button").on("click", function(){
+	var $gnbBtn = $(".gnb>button");
+	$gnbBtn.on("click", function(){
 		//$(".gnb>ul").slideToggle(200);
-		$(this).next("ul").slideToggle(200);
+		$gnbBtn.next("ul").slideToggle(200);
 		//$(this).next().slideToggle(200);
+		if($gnbBtn.text() == "메뉴 열기"){
+			$gnbBtn.text("메뉴 닫기");
+		} else {
+			$gnbBtn.text("메뉴 열기");
+		}
+		
 	});
 });
